46c6d4dd01d7b6874aa5333cd7f586de6625f031
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-17 20:29:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-window.promises = [];
+let promise1 = new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve();
+	},1000)
+});
+let promise2 = new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve();
+	},2000)
+});
+let promise3 = new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve();
+	},3000)
+});
+let promise4 = new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve();
+	},4000)
+});
+let promise5 = new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve();
+	},5000)
+});
+window.promises = [promise1,promise2,promise3,promise4,promise5];
+const output = document.getElementById("output");
+output.innerText=Promise.any(promises);
 
 // Do not change the code above this
 // add your promises to the array `promises`
